0fb5bbc130b361ed5dce8d3c93a0a8e22727feec
Ayrton <ayrtoncravero26@gmail.com>
2021-12-08 14:41:47 -0300

    adding method for findAll products and inyection od dependency

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,6 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('api/users')
 export class UsersController {
-
+    @Get()
+    getAll() {
+        
+    };
 }
