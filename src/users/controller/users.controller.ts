e978de9db613df49fd61efab1e352aae414e0467
Ayrton <ayrtoncravero26@gmail.com>
2021-12-08 15:30:50 -0300

    adding method getOne

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from '../service/users.service';
 
 @Controller('api/users')
@@ -16,6 +16,15 @@ export class UsersController {
     create(@Body() body: any) {
         try {
             return this.UserService.create(body);
+        } catch(error) {
+            return error;
+        };
+    };
+
+    @Get(':id')
+    getOne(@Param('id') id: number) {
+        try {
+            return this.UserService.findOne(id);
         } catch(error) {
             return error;
         };
