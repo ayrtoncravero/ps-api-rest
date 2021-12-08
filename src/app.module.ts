a40f78eefeccda1ca70d5fd2e4ab3b8a8f9fb5ff
Ayrton <ayrtoncravero26@gmail.com>
2021-12-08 12:03:37 -0300

    adding configuration for typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'irton',
+      password: '9371',
+      database: 'ps_2021',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
