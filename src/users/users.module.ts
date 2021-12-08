86c4849dbeb1c6fbced3ed81f9dd4d042d51d352
Ayrton <ayrtoncravero26@gmail.com>
2021-12-08 13:58:07 -0300

    adding configuration of typeOrm for work typeOrm with 'User'

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controller/users.controller';
 import { UsersService } from './service/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [UsersController],
   providers: [UsersService]
 })
