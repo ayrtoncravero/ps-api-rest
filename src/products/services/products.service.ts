68be71ef1931e245b215551321719cc0c4edcd28
Ayrton <ayrtoncravero26@gmail.com>
2021-12-08 16:06:13 -0300

    adding inyection dependecy for repositoryProduct

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from '../entities/product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(
+        @InjectRepository(Product)
+        private ProductRepository: Repository<Product>
+    ) {};
+}
