065c8ed28c82b927a6aad3b4fc5e16230a04771f
Ayrton <ayrtoncravero26@gmail.com>
2021-12-15 14:55:27 -0300

    add all documentation of 'user' for 'swagger'

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Get, Post, Param, Put, Delete } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Put, Delete, HttpCode } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
+import { 
+    ApiTags,
+    ApiResponse,
+    ApiCreatedResponse,
+    ApiOperation,
+    ApiBadRequestResponse,
+    ApiBody,
+} from '@nestjs/swagger';
+import { User } from '../entities/user.entity';
 
+@ApiTags('users')
 @Controller('api/users')
 export class UsersController {
     constructor(
@@ -8,6 +18,14 @@ export class UsersController {
     ) {};
 
     @Get()
+    @ApiOperation({
+        summary: 'Obtiene todos los usuarios.'
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'Una lista con todos los usuarios.',
+        type: User,
+    })
     getAll() {
         try {
             return this.UserService.findAll();
@@ -17,6 +35,24 @@ export class UsersController {
     }
 
     @Post() 
+    @HttpCode(201)
+    @ApiOperation({
+        summary: 'Crear un usuario.',
+    })
+    @ApiResponse({
+        status: 201,
+        description: 'El usuario fue creado correctamente.',
+    })
+    @ApiBadRequestResponse({
+        description: 'El usuario no pudo ser creado.',
+    })
+    @ApiCreatedResponse({
+        description: 'El usuario fue creado correctamente',
+        type: User,
+    })
+    @ApiBody({
+        type: User,
+    })
     create(@Body() body: any) {
         try {
             return this.UserService.create(body);    
@@ -25,15 +61,19 @@ export class UsersController {
         };
     };
 
-    /* @Get(':id')
-    getOne(@Param('id') id: number) {
-        try {
-            return this.UserService.findOne(id);
-        } catch(error) {
-            return `${error}`; 
-        };
-    }; */
     @Get(':id')
+    @HttpCode(200)
+    @ApiOperation({
+        summary: 'Obtiene un usuario por id.',
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'Usuario encontrado.',
+        type: User,
+    })
+    @ApiBadRequestResponse({
+        description: 'El usuario no pudo encontrarse.'
+    })
     getOne(@Param('id') id: number) {
         try {
             return this.UserService.findOne(id);
@@ -43,6 +83,20 @@ export class UsersController {
     };
 
     @Put(':id')
+    @HttpCode(200)
+    @ApiOperation({
+        summary: 'Actualizacion de usuario.'
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'El usuario se actualizo correctamente.',
+    })
+    @ApiBadRequestResponse({
+        description: 'No se pudo encontrar el producto.',
+    })
+    @ApiBody({
+        type: User,
+    })
     update(@Param('id') id: number, @Body() body: any) {
         try {
             return this.UserService.update(id, body);
@@ -52,6 +106,17 @@ export class UsersController {
     };
 
     @Delete(':id')
+    @HttpCode(204)
+    @ApiOperation({
+        summary: 'Eliminacion de un usuario.'
+    })
+    @ApiResponse({
+        status:200,
+        description: 'El usuario fue eliminado con exito.',
+    })
+    @ApiBadRequestResponse({
+        description: 'El usuario no pudo eliminarse.',
+    })
     async delete(@Param('id') id: number) {
         try {
             return await this.UserService.delete(id);
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,37 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
+    @ApiProperty({
+        example: 1,
+        description: 'Identificador unico del usuario.',
+        type: Number,
+    })
     id: number;
 
     @Column()
+    @ApiProperty({
+        example: 'pepito',
+        description: 'Representa el nombre del usuario.',
+        type: String,
+    })
     name: string;
 
     @Column()
+    @ApiProperty({
+        example: 'perez',
+        description: 'Representa el apellido del usuario.',
+        type: String,
+    })
     surName: string;
 
     @Column()
+    @ApiProperty({
+        example: 'pepito@example.com',
+        description: 'Representa el correo electronico del usuario.',
+        type: String,
+    })
     email: string;
 }
